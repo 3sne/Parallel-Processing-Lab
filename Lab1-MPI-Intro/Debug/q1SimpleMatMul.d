eaca576da5385bff81172ac25efe6596d85fafd0
Mukur Panchani <panchanimukur@gmail.com>
2019-02-26 13:41:56 +0530

    Remove executables

--- Lab1-MPI-Intro/Debug/q1SimpleMatMul.d
+++
@@ -1,8 +0,0 @@
-q1SimpleMatMul.d: ../q1SimpleMatMul.c /usr/include/mpich/mpi.h \
- /usr/include/mpich/mpio.h /usr/include/mpich/mpi.h
-
-/usr/include/mpich/mpi.h:
-
-/usr/include/mpich/mpio.h:
-
-/usr/include/mpich/mpi.h:
--- Lab1-MPI-Intro/Debug/q2MpiHelloWorld.d
+++
@@ -1,8 +0,0 @@
-q2MpiHelloWorld.d: ../q2MpiHelloWorld.c /usr/include/mpich/mpi.h \
- /usr/include/mpich/mpio.h /usr/include/mpich/mpi.h
-
-/usr/include/mpich/mpi.h:
-
-/usr/include/mpich/mpio.h:
-
-/usr/include/mpich/mpi.h:
